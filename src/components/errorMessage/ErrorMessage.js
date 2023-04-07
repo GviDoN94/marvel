0b8395fbc2d49b6e7221666f7ac51c91f98f4f7b
Vladislav Surin <vlad_surin@mail.ru>
2023-04-07 23:49:52 +0300

    The card of a random hero is updated on click. Implemented error handling for invalid request

---
+++ src/components/errorMessage/ErrorMessage.js
@@ -0,0 +1,10 @@
+import './errorMessage.scss';
+import img from './error.gif';
+
+const ErrorMessage = () => {
+    return (
+        <img className='errorMessage' src={img} alt="Error" />
+    )
+}
+
+export default ErrorMessage;
